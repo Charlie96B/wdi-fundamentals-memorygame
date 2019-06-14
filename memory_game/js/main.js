62510684cecad66f0b97dbac34c2ecfa92e1eaed
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-06-14 14:11:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,33 +1,36 @@
 console.log("Up and running!");
 
-/*
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-*/
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2) {
+function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found the first match!");
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
 
-/*
-console.log ("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
-*/
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	/*
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found the first match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+	}
+	*/
+}
 
-console.log("User flipped queen");
-console.log("User flipped king");
+flipCard(0);
+flipCard(2);
+//console.log("User flipped queen");
+//console.log("User flipped king");
